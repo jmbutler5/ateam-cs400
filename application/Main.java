62d71fc8c139e3735e2a21dec20e21e9b4fa266c
jmbutler5 <56049748+jmbutler5@users.noreply.github.com>
2019-11-29 19:33:40 -0600

    Allowed file interaction
    I added SocialNetwork.readFromFile(String), SocialNetwork.saveLog(String), SocialNetwork.undo(), and some helper functions. I also moved the selected user to SocialNetwork.java because it made it more convenient for me, and that's how it was in social-network-design.pdf. I haven't actually tested it because the Graph isn't implemented yet, so there's probably some bugs.

--- application/Main.java
+++ application/Main.java
@@ -1,8 +1,13 @@
 package application;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import javafx.application.Application;
 import javafx.collections.FXCollections;
@@ -30,59 +35,62 @@ public class Main extends Application {
 	// NOTE: this.getParameters().getRaw() will get these also
 	private List<String> args;
 
+	private SocialNetwork socialNetwork = new SocialNetwork();
+
 	private static final int WINDOW_WIDTH = 400;
 	private static final int WINDOW_HEIGHT = 400;
 	private static final String APP_TITLE = "Social Network";
 	// Center of the GUI (menuBox and networkBox) are arranged on a grid
 	private GridPane rootGrid = new GridPane();
-	
+
 	// rootGrid and other boxes are arranged on BorderPane
 	private BorderPane rootPane = new BorderPane();
-	
-	private String selectedUser;
+
+	// the user selected which might become the selected user
 	private String currentUser;
+
+
 	
+
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// save args example
 		args = this.getParameters().getRaw();
-			
-		
 
-		//Set up main Gridpane
+		// Set up main Gridpane
 		rootGrid.setPadding(new Insets(10, 10, 10, 10));
 		rootGrid.setVgap(5);
 		rootGrid.setHgap(5);
-		
-		//Add the main grid to the BorderPane
+
+		// Add the main grid to the BorderPane
 		rootPane.setCenter(rootGrid);
-		
-		
-		//root.setBottom(new Button("Done"));
+
+		// root.setBottom(new Button("Done"));
 		Scene mainScene = new Scene(rootPane, WINDOW_WIDTH, WINDOW_HEIGHT);
-		
+
 		topBox();
 		menuBox();
 		networkBox();
-		
+
 		// Generate imageview control instance and add to window
-		//root.setCenter(newImage("headshot.jpg"));
+		// root.setCenter(newImage("headshot.jpg"));
 		// Add the stuff and set the primary stage
 		primaryStage.setTitle(APP_TITLE);
-        	primaryStage.setScene(mainScene);
-        	primaryStage.show();
+		primaryStage.setScene(mainScene);
+		primaryStage.show();
 	}
-	
+
 	/**
 	 * Method to set up the top bar of the GUI
-	 * @throws FileNotFoundException 
+	 * 
+	 * @throws FileNotFoundException
 	 */
 	private void topBox() throws FileNotFoundException {
 		rootPane.setTop(newImage("socialnetwork.jpg"));
 		BorderPane.setAlignment(rootPane, Pos.TOP_RIGHT);
-		BorderPane.setMargin(rootPane, new Insets(12,12,12,12));
+		BorderPane.setMargin(rootPane, new Insets(12, 12, 12, 12));
 	}
-	
+
 	/**
 	 * Method to draw the menu box on the left side of the GUI main screen
 	 */
@@ -92,32 +100,31 @@ public class Main extends Application {
 		grid1.setPadding(new Insets(10, 10, 10, 10));
 		grid1.setVgap(5);
 		grid1.setHgap(5);
-		
+
 		// Grid for menu options
 		GridPane grid2 = new GridPane();
-		
-		//grid2.setPadding(new Insets(10, 10, 10, 10));
+
+		// grid2.setPadding(new Insets(10, 10, 10, 10));
 		grid2.setVgap(5);
 		grid2.setHgap(5);
-		
+
 		// Search Box
 		Label l1 = new Label("Search: ");
 		TextField search = new TextField();
 		search.setPromptText("Enter username");
 		search.getText();
 		search.setPrefWidth(100);
-		//grid1Pane.setConstraints(search, 0, 0);
+		// grid1Pane.setConstraints(search, 0, 0);
 
 		// Menu Options
 		Label l2 = new Label("Selected User: ");
-		
+
 		// TODO: Replace with dynamic label that displays the current user//
-		selectedUser = "JohnDoe";
+		//selectedUser = "JohnDoe";
 		currentUser = "JaneDoe";
-		Label l3 = new Label(selectedUser);
+		Label l3 = new Label(socialNetwork.getSelected());
 		//////////////////////////////////////////////////////////////////
-	
-		
+
 		Button b1 = new Button("Shortest path to " + currentUser);
 		Button b2 = new Button("Show Mutual Friends");
 		Button b3 = new Button("Set " + currentUser + " as center");
@@ -127,21 +134,21 @@ public class Main extends Application {
 		b2.setPrefWidth(buttonWidth);
 		b3.setPrefWidth(buttonWidth);
 		b4.setPrefWidth(buttonWidth);
-		
+
 		// Set up Grid 1
 		grid1.add(l1, 0, 0);
 		grid1.add(l2, 0, 1);
 		grid1.add(l3, 1, 1);
 		grid1.add(search, 1, 0);
 		GridPane.setHalignment(l1, HPos.RIGHT);
-		
+
 		// Set up Grid 2
 		grid2.add(b1, 0, 2);
 		grid2.add(b2, 0, 3);
 		grid2.add(b3, 0, 4);
 		grid2.add(b4, 0, 5);
 		grid2.setAlignment(Pos.CENTER);
-		
+
 		// Combine grids in VBox
 		VBox v1 = new VBox();
 		v1.getChildren().add(grid1);
@@ -150,29 +157,28 @@ public class Main extends Application {
 		// Add Vbox to the main gridpane
 		rootGrid.add(v1, 0, 1);
 	}
-	
+
 	/**
-	 * Method to draw the friend network of the currently selected person
-	 * on the right side of the GUI main screen
+	 * Method to draw the friend network of the currently selected person on the
+	 * right side of the GUI main screen
 	 */
 	void networkBox() {
 		GridPane grid1 = new GridPane();
 		grid1.setPadding(new Insets(10, 10, 10, 10));
 		grid1.setVgap(5);
 		grid1.setHgap(5);
-		
-		
+
 		// Make these dynamic based on currently selected user and function
 		Label l1 = new Label("Current User: " + currentUser);
 		Label l2 = new Label("Showing: Mutual Friends");
 		//////////////////////////////////////////////////////////////////
-		
+
 		grid1.add(l1, 0, 0);
 		grid1.add(l2, 0, 1);
-		
+
 		//////////////////////////////////////////////////////////////////
 		// Make displayed data a VBox which is inserted into the Grid
-		
+
 		// TODO: determine the functionality to generate a list of friends
 		// and add them to the GUI. Likely will want to use a helper method
 		// which returns a list regardless of if a list or a set is returned,as
@@ -181,51 +187,54 @@ public class Main extends Application {
 //		for(Person person : displayList) {
 //			
 //		}
-		
+
 		// Hardcoded display of mutual friends as example
-		Label l3  = new Label("MaryAnn");
+		Label l3 = new Label("MaryAnn");
 		Label l4 = new Label("JimJohn");
 		Label l5 = new Label("Jimothy");
 		Label l6 = new Label("Janthony");
 		VBox v1 = new VBox();
-		v1.getChildren().addAll(l3,l4,l5,l6);
+		v1.getChildren().addAll(l3, l4, l5, l6);
 		v1.setAlignment(Pos.CENTER);
 		grid1.add(v1, 0, 2);
 		grid1.setAlignment(Pos.CENTER);
-		
-		
-		
+
 		rootGrid.add(grid1, 1, 1);
 	}
-	
+
 	/**
 	 * Draws the status box on the bottom of the main GUI screen
 	 */
 	void statusBox() {
-		
+
 	}
-	
-	
+
 	/**
 	 * Class which handles UI interaction with button
+	 * 
 	 * @author john
 	 *
 	 */
 	class MyHandler implements EventHandler<ActionEvent> {
 		Button button;
-		MyHandler(Button button) {this.button = button;}
+
+		MyHandler(Button button) {
+			this.button = button;
+		}
+
 		@Override
 		public void handle(ActionEvent e) {
-			if(button.getText().equals("Change to Nature Scene")) {
+			if (button.getText().equals("Change to Nature Scene")) {
 				button.setText("Change to headshot");
-			}
-			else
+			} else
 				button.setText("Change to Nature Scene");
 		}
 	}
+
 	/**
 	 * Private class generates an ImageView object from a file
-	 * @throws FileNotFoundException 
+	 * 
+	 * @throws FileNotFoundException
 	 */
 	private ImageView newImage(String filePath) throws FileNotFoundException {
 		FileInputStream input = new FileInputStream(filePath);
@@ -234,14 +243,13 @@ public class Main extends Application {
 		imageView.setFitWidth(400);
 		imageView.setPreserveRatio(true);
 		return imageView;
-		
-		
+
 	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		   launch(args);
+		launch(args);
 	}
 }
--- application/SocialNetwork.java
+++ application/SocialNetwork.java
@@ -1,27 +1,57 @@
 package application;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class SocialNetwork {
-	
+
+	// the user being displayed
+	//TODO hardcoded
+	private UserStruct selectedUser = new User("John Doe");
+
+	// keeps track of the log so we can print it to a file
+	// we have to actually keep track of operations (i.e. undo), so we can't just
+	// print a representation of the network
+	private ArrayList<String> log;
+
+	private Graph graph;
+
+	// these are the commands for the input file specified in the problem
+	// description here:
+	// https://canvas.wisc.edu/courses/170796/pages/team-project-social-network
+	private static final char ADD_COMMAND = 'a';
+	private static final char REMOVE_COMMAND = 'r';
+	private static final char SET_COMMAND = 's';
+
+	public SocialNetwork() {
+		log = new ArrayList<String>();
+		graph = new Graph();
+	}
+
 	/**
 	 * adds the specified user to the network
 	 * 
 	 * @param user the name of the user to be added to the network
 	 */
 	void addUser(String user) {
-		//TODO
+		// TODO
+		log.add(ADD_COMMAND + " " + user);
 	}
-	
+
 	/**
 	 * removes the specified user from the network
 	 * 
 	 * @param user the name of the user to be removed from the network
 	 */
 	void removeUser(String user) {
-		//TODO
+		// TODO
+		log.add(REMOVE_COMMAND + " " + user);
 	}
-	
+
 	/**
 	 * Makes user1 and user2 friends in the network
 	 * 
@@ -29,19 +59,31 @@ public class SocialNetwork {
 	 * @param user2 the name of the other user to become friends
 	 */
 	void addFriend(String user1, String user2) {
-		//TODO
+		// TODO
+		log.add(ADD_COMMAND + " " + user1 + " " + user2);
 	}
-	
+
+	/**
+	 * ends the friendship between these two users
+	 * 
+	 * @param user1 the name of one user in the network
+	 * @param user2 the name of another user in the network
+	 */
+	void removeFriend(String user1, String user2) {
+		// TODO
+		log.add(REMOVE_COMMAND + " " + user1 + " " + user2);
+	}
+
 	/**
 	 * finds the number of connected groups in the network
-	 *  
+	 * 
 	 * @return returns the number of connected groups in the network
 	 */
 	public int getGroups() {
-		//TODO
+		// TODO
 		return 0;
 	}
-	
+
 	/**
 	 * finds the list of mutual friends of user1 and user 2
 	 * 
@@ -49,31 +91,262 @@ public class SocialNetwork {
 	 * @param user2 the name of another user in the graph
 	 * @return returns a List of users who have user1 and user2 as friends
 	 */
-	public List<UserStruct> mutualFriends(String user1, String user2){
-		//TODO
+	public List<UserStruct> mutualFriends(String user1, String user2) {
+		// TODO
 		return null;
 	}
-	
+
 	/**
 	 * finds the shorted path between user1 and user2
 	 * 
 	 * @param user1 the name of a user in the graph
 	 * @param user2 the name of another user in the graph
-	 * @return returns a List which is the shortest path from user1 to user2 in the network
+	 * @return returns a List which is the shortest path from user1 to user2 in the
+	 *         network
 	 */
-	public List<UserStruct> friendLink(String user1, String user2){
-		//TODO
+	public List<UserStruct> friendLink(String user1, String user2) {
+		// TODO
 		return null;
 	}
-	
+
 	/**
 	 * finds all friends of the specified user
 	 * 
 	 * @param user the user whose friends will be found
 	 * @return returns a list of people who are friends with user
 	 */
-	public List<UserStruct> allFriends(String user){
-		//TODO
+	public List<UserStruct> allFriends(String user) {
+		// TODO
 		return null;
+	}
+
+	public String getSelected() {
+		return selectedUser.getName();
+	}
+
+	//TODO we might want to select the user by the actual instance instead of their name.
+	public boolean select(String user) {
+		if(graph.contains(new User(user))) {
+			//TODO
+		}
+		return false;
+	}
+
+	// This undo is cyclic. i.e. if you add a connection and keep calling undo(), it
+	// will keep adding and removing that connection
+	public String undo() {
+		if (log.size() == 0)
+			return "There was no action to undo";
+		
+		String lastCommand = log.get(log.size() - 1);
+		String message = "";
+		
+		switch (lastCommand.charAt(0)) {
+		case ADD_COMMAND:
+			// the opposite of adding is removing
+			lastCommand = REMOVE_COMMAND + lastCommand.substring(1);
+			
+			// TODO I don't check remove the person if they were added because of the
+			// connection
+			
+			message = processCommand(lastCommand);
+
+			// if the last command was invalid, it will be stopped here
+			if (message.equals(""))
+				log.add(lastCommand);
+			return "The previous command was invalid: " + lastCommand;
+
+		case REMOVE_COMMAND:
+			// the opposite of removing is adding
+			lastCommand = ADD_COMMAND + lastCommand.substring(1);
+			message = processCommand(lastCommand);
+
+			// if the last command was invalid, it will be stopped here
+			if (message.equals(""))
+				log.add(lastCommand);
+			return "the previous command was invalid: " + lastCommand;
+
+		case SET_COMMAND:
+
+			// I have to search for the last person who was selected
+			for (int i = log.size() - 2; i >= 0; i--) {
+				if (log.get(i).charAt(0) == SET_COMMAND) {
+
+					// once found, select that person
+					lastCommand = log.get(i);
+					message = processCommand(lastCommand);
+
+					// I don't actually check if the previous command was valid in this case, but I
+					// don't think that there is any way for there to be an invalid command
+					if (message.equals(""))
+						log.add(lastCommand);
+					return message;
+				}
+			}
+			//if I never found another set command in the rest of the log
+			return "There was no one previously selected.";
+		default:
+			return "invalid previous command: " + lastCommand;
+		}
+	}
+
+	/**
+	 * helper function to processCommand(String), it parses a command and finds the
+	 * parameters. This validates the input as well
+	 * 
+	 * @author John Butler
+	 * 
+	 * @param command the command to be parsed
+	 * @return an array of the parameters if the command is valid, null otherwise
+	 */
+	private String[] getParameters(String command) {
+		// makes sure there is a command to process
+		if (command.equals(""))
+			return null;
+		String[] tokens = command.split(" ");
+
+		// the command part must be one character only
+		if (tokens[0].length() != 1)
+			return null;
+
+		// these are the only valid commands
+		// I check for the correct number of parameters for the command as well:
+		// add and remove can have one or two parameters, while set can only have one
+		switch (tokens[0].charAt(0)) {
+		case ADD_COMMAND:
+		case REMOVE_COMMAND:
+			if (tokens.length == 3) // command + two parameters
+				return new String[] { tokens[1], tokens[2] };
+		case SET_COMMAND:
+			if (tokens.length == 2) // command + one parameter
+				return new String[] { tokens[1] };
+		default:
+			break;
+		}
+
+		// this line is only reached if there is an invalid number of parameters
+		return null;
+	}
+
+	private String processCommand(String command) {
+		return processCommand(command, this);
+	}
+
+	/**
+	 * Performs the operation specified in command on network in case we only want
+	 * to use a temporary network.
+	 * 
+	 * @author John Butler
+	 * 
+	 * @param command the command to be processed
+	 * @param network the network to apply the command to
+	 * @return returns an error message if the command is invalid, otherwise returns
+	 *         an empty string
+	 */
+	private String processCommand(String command, SocialNetwork network) {
+
+		String[] parameters = getParameters(command);
+
+		// if the data does not get processed correctly,
+		// getParameters(String) returns null
+		if (parameters == null)
+			return "unable to process parameters.";
+
+		// I already checked for the number of parameters in getParameters(String), so I
+		// don't have to worry about that here
+		switch (command.charAt(0)) {
+		case ADD_COMMAND:
+			// the specific action taken is dependent on how many parameters there are
+			if (parameters.length == 1)
+				network.addUser(parameters[0]);
+			else
+				network.addFriend(parameters[0], parameters[1]);
+			break;
+		case REMOVE_COMMAND:
+			// the specific action taken is dependent on how many parameters there are
+			if (parameters.length == 1)
+				network.removeUser(parameters[0]);
+			else
+				network.removeFriend(parameters[0], parameters[1]);
+
+			break;
+		case SET_COMMAND:
+			selectedUser = new User(parameters[0]);
+			break;
+		default:
+			// getParameters(String) already validates the command input, but just in case
+			return command.charAt(0) + "' is an invalid comand";
+		}
+		return "";
+	}
+
+	/**
+	 * replaces the current social network with what's found in the file. If the
+	 * file is invalid, the social network is not overwritten. What is considered
+	 * valid is specified here:
+	 * https://canvas.wisc.edu/courses/170796/pages/team-project-social-network
+	 * 
+	 * @author John Butler
+	 * 
+	 * @param file the path to a file containing the commands in plain text
+	 * @return returns an error message if the data in the file is invalid, an empty
+	 *         string otherwise
+	 */
+	private String readFromFile(String filename) {
+		// TODO I'm waiting to test this until SocialNetwork is done
+		try {
+			Scanner input = new Scanner(new File(filename));
+			// I use these temporary variables so that I don't overwrite the real variables
+			// if something goes wrong
+			SocialNetwork tempNetwork = new SocialNetwork();
+			ArrayList<String> tempLog = new ArrayList<String>();
+
+			int currentLine = 1;
+			while (input.hasNext()) {
+				String command = input.nextLine();
+				String invalidCommandMessage = "invalid command at line " + currentLine++ + " in inputted file: \""
+						+ command + '"';
+				tempLog.add(command);
+				String message = processCommand(command, tempNetwork);
+				if (!message.equals("")) {
+					input.close();
+					return invalidCommandMessage + ", " + message;
+				}
+			}
+
+			// after we confirm that there were no errors, we can safely process the
+			// commands on this instance
+			log = tempLog;
+			for (String command : log)
+				processCommand(command);
+
+			input.close();
+		} catch (FileNotFoundException e) {
+			return "File \"" + filename + "\" not found.";
+		}
+		return "";
+	}
+
+	/**
+	 * 
+	 * @author John Butler
+	 * 
+	 * @param filename
+	 * @return returns true if the save was successful, false otherwise
+	 */
+	private boolean saveLog(String filename) {
+		// TODO I'm waiting to test this until SocialNetwork is done
+
+		PrintWriter writer;
+		try {
+			writer = new PrintWriter(filename);
+			for (String command : log)
+				writer.println(command);
+
+			writer.close();
+		} catch (FileNotFoundException e) {
+			return false;
+		}
+		return true;
 	}
 }
