2bbedb4dd103092dcbb1f70c741a3ee6c2cd0062
Aileen Lee <aileen@Aileens-MacBook-Pro.local>
2019-12-10 13:10:49 -0600

    revert till connor adds

--- application/Main.java
+++ application/Main.java
@@ -24,323 +24,329 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-	// store any command-line arguments that were entered.
-	// NOTE: this.getParameters().getRaw() will get these also
-	private List<String> args;
+    // store any command-line arguments that were entered.
+    // NOTE: this.getParameters().getRaw() will get these also
+    private List<String> args;
 
-	private SocialNetwork socialNetwork = new SocialNetwork();
+    private SocialNetwork socialNetwork = new SocialNetwork();
 
-	private static final int WINDOW_WIDTH = 400;
-	private static final int WINDOW_HEIGHT = 400;
-	private static final String APP_TITLE = "Social Network";
-	// Center of the GUI (menuBox and networkBox) are arranged on a grid
-	private GridPane rootGrid = new GridPane();
+    private static final int WINDOW_WIDTH = 400;
+    private static final int WINDOW_HEIGHT = 400;
+    private static final String APP_TITLE = "Social Network";
+    // Center of the GUI (menuBox and networkBox) are arranged on a grid
+    private GridPane rootGrid = new GridPane();
 
-	// rootGrid and other boxes are arranged on BorderPane
-	private BorderPane startPane = new BorderPane();
-	private BorderPane mainPane = new BorderPane();
-	private BorderPane exitPane = new BorderPane();
+    // rootGrid and other boxes are arranged on BorderPane
+    private BorderPane startPane = new BorderPane();
+    private BorderPane mainPane = new BorderPane();
+    private BorderPane exitPane = new BorderPane();
 
-	// the user selected which might become the selected user
-	private String currentUser;
+    // the user selected which might become the selected user
+    private String currentUser;
 
-	Scene startScene, mainScene, exitScene;
+    Scene startScene, mainScene, exitScene;
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		// Welcome Scene
-		HBox fileNameBox = new HBox();
-		fileNameBox.setSpacing(5);
-		TextField fileName = new TextField();
-		Label label = new Label("Enter file name to read from: ");
+    @Override
+    public void start(Stage primaryStage) throws Exception {
 
-		fileNameBox.getChildren().addAll(label, fileName);
+        // Welcome Scene
+        welcome();
+        // add confirm button (move from start scene to main scene)
+        VBox confirm = new VBox();
+        Button toMain = new Button("Confirm");
+        toMain.setOnAction(e -> primaryStage.setScene(mainScene)); // on click - goes to mainScene
+        confirm.getChildren().add(toMain);
+        startPane.setBottom(confirm);
 
-		startPane.setPadding(new Insets(10, 10, 10, 10));
+        startScene = new Scene(startPane, WINDOW_WIDTH, WINDOW_HEIGHT);
 
-		startPane.setCenter(fileNameBox);
-		// add confirm button (move from start scene to main scene)
-		VBox confirm = new VBox();
-		Button toMain = new Button("Confirm");
-		// when "Confirm" Button is pressed
-		// 1. input fileName is read from textField
-		// 2. goes to mainScene
-		toMain.setOnAction(e -> {
-			socialNetwork.readFromFile(fileName.getText());
-			primaryStage.setScene(mainScene);
-		});
+        // Main Scene
+        // save args example
+        args = this.getParameters().getRaw();
 
-		confirm.getChildren().add(toMain);
-		startPane.setBottom(confirm);
+        // Set up main Gridpane
+        rootGrid.setPadding(new Insets(10, 10, 10, 10));
+        rootGrid.setVgap(5);
+        rootGrid.setHgap(5);
 
-		startScene = new Scene(startPane, WINDOW_WIDTH, WINDOW_HEIGHT);
+        // Add the main grid to the BorderPane
+        mainPane.setCenter(rootGrid);
 
-		// Main Scene
-		// save args example
-		args = this.getParameters().getRaw();
+        // add exit button
+        VBox bottom = new VBox();
+        Button toExit = new Button("Done");
+        toExit.setOnAction(e -> primaryStage.setScene(exitScene)); // on click - goes to exitScene
+        bottom.getChildren().add(toExit);
 
-		// Set up main Gridpane
-		rootGrid.setPadding(new Insets(10, 10, 10, 10));
-		rootGrid.setVgap(5);
-		rootGrid.setHgap(5);
+        // add undo button
+        Button undo = new Button("Undo");
+        undo.setOnAction(e -> {
+            String message = socialNetwork.undo();
+            if (!message.equals("")) {
+                // TODO display error
+            } else {
+                // TODO reset display
+            }
+        });
+        bottom.getChildren().add(undo);
 
-		// Add the main grid to the BorderPane
-		mainPane.setCenter(rootGrid);
+        mainPane.setBottom(bottom);
 
-		// add exit button
-		VBox bottom = new VBox();
-		Button toExit = new Button("Done");
-		toExit.setOnAction(e -> primaryStage.setScene(exitScene)); // on click - goes to exitScene
-		bottom.getChildren().add(toExit);
+        mainScene = new Scene(mainPane, WINDOW_WIDTH, WINDOW_HEIGHT);
 
-		// add undo button
-		Button undo = new Button("Undo");
-		undo.setOnAction(e -> {
-			String message = socialNetwork.undo();
-			if (!message.equals("")) {
-				// TODO display error
-			} else {
-				// TODO reset display
-			}
-		});
-		bottom.getChildren().add(undo);
+        topBox();
+        menuBox();
+        networkBox();
 
-		mainPane.setBottom(bottom);
+        // Exiting Scene
+        exitScene = new Scene(exitPane, WINDOW_WIDTH, WINDOW_HEIGHT);
+        exiting();
 
-		mainScene = new Scene(mainPane, WINDOW_WIDTH, WINDOW_HEIGHT);
+        // Generate imageview control instance and add to window
+        // startPane.setCenter(newImage("headshot.jpg"));
+        // Add the stuff and set the primary stage
+        primaryStage.setTitle(APP_TITLE);
+        primaryStage.setScene(startScene);
+        primaryStage.show();
+    }
 
-		topBox();
-		menuBox();
-		networkBox();
+    /**
+     * Method to set up the top bar of the GUI
+     * 
+     * @throws FileNotFoundException
+     */
+    private void topBox() throws FileNotFoundException {
+        mainPane.setTop(newImage("socialnetwork.png"));
+        BorderPane.setAlignment(mainPane, Pos.TOP_RIGHT);
+        BorderPane.setMargin(mainPane, new Insets(12, 12, 12, 12));
+    }
 
-		// Exiting Scene
-		exitScene = new Scene(exitPane, WINDOW_WIDTH, WINDOW_HEIGHT);
-		exiting();
+    /**
+     * Method to draw the menu box on the left side of the GUI main screen
+     */
+    private void menuBox() {
+        // Grid for search bar and selected user
+        GridPane grid1 = new GridPane();
+        grid1.setPadding(new Insets(10, 10, 10, 10));
+        grid1.setVgap(5);
+        grid1.setHgap(5);
 
-		// Generate imageview control instance and add to window
-		// startPane.setCenter(newImage("headshot.jpg"));
-		// Add the stuff and set the primary stage
-		primaryStage.setTitle(APP_TITLE);
-		primaryStage.setScene(startScene);
-		primaryStage.show();
-	}
+        // Grid for menu options
+        GridPane grid2 = new GridPane();
 
-	/**
-	 * Method to set up the top bar of the GUI
-	 * 
-	 * @throws FileNotFoundException
-	 */
-	private void topBox() throws FileNotFoundException {
-		mainPane.setTop(newImage("socialnetwork.png"));
-		BorderPane.setAlignment(mainPane, Pos.TOP_RIGHT);
-		BorderPane.setMargin(mainPane, new Insets(12, 12, 12, 12));
-	}
+        // grid2.setPadding(new Insets(10, 10, 10, 10));
+        grid2.setVgap(5);
+        grid2.setHgap(5);
 
-	/**
-	 * Method to draw the menu box on the left side of the GUI main screen
-	 */
-	private void menuBox() {
-		// Grid for search bar and selected user
-		GridPane grid1 = new GridPane();
-		grid1.setPadding(new Insets(10, 10, 10, 10));
-		grid1.setVgap(5);
-		grid1.setHgap(5);
+        // Search Box
+        Label l1 = new Label("Search: ");
+        TextField search = new TextField();
+        search.setPromptText("Enter username");
+        search.getText();
+        search.setPrefWidth(100);
+        // grid1Pane.setConstraints(search, 0, 0);
 
-		// Grid for menu options
-		GridPane grid2 = new GridPane();
+        // Menu Options
+        Label l2 = new Label("Selected User: ");
 
-		// grid2.setPadding(new Insets(10, 10, 10, 10));
-		grid2.setVgap(5);
-		grid2.setHgap(5);
+        // TODO: Replace with dynamic label that displays the current user//
+        // selectedUser = "JohnDoe";
+        currentUser = "JaneDoe";
+        Label l3 = new Label(socialNetwork.getSelected());
+        //////////////////////////////////////////////////////////////////
 
-		// Search Box
-		Label l1 = new Label("Search: ");
-		TextField search = new TextField();
-		search.setPromptText("Enter username");
-		search.getText();
-		search.setPrefWidth(100);
-		// grid1Pane.setConstraints(search, 0, 0);
+        Button b1 = new Button("Shortest path to " + currentUser);
+        Button b2 = new Button("Show Mutual Friends");
+        Button b3 = new Button("Set " + currentUser + " as center");
+        Button b4 = new Button("Find Connected Components");
+        int buttonWidth = 200;
+        b1.setPrefWidth(buttonWidth);
+        b2.setPrefWidth(buttonWidth);
+        b3.setPrefWidth(buttonWidth);
+        b4.setPrefWidth(buttonWidth);
 
-		// Menu Options
-		Label l2 = new Label("Selected User: ");
+        // Set up Grid 1
+        grid1.add(l1, 0, 0);
+        grid1.add(l2, 0, 1);
+        grid1.add(l3, 1, 1);
+        grid1.add(search, 1, 0);
+        GridPane.setHalignment(l1, HPos.RIGHT);
 
-		// TODO: Replace with dynamic label that displays the current user//
-		// selectedUser = "JohnDoe";
-		currentUser = "JaneDoe";
-		Label l3 = new Label(socialNetwork.getSelected());
-		//////////////////////////////////////////////////////////////////
+        // Set up Grid 2
+        grid2.add(b1, 0, 2);
+        grid2.add(b2, 0, 3);
+        grid2.add(b3, 0, 4);
+        grid2.add(b4, 0, 5);
+        grid2.setAlignment(Pos.CENTER);
 
-		Button b1 = new Button("Shortest path to " + currentUser);
-		Button b2 = new Button("Show Mutual Friends");
-		Button b3 = new Button("Set " + currentUser + " as center");
-		Button b4 = new Button("Find Connected Components");
-		int buttonWidth = 200;
-		b1.setPrefWidth(buttonWidth);
-		b2.setPrefWidth(buttonWidth);
-		b3.setPrefWidth(buttonWidth);
-		b4.setPrefWidth(buttonWidth);
+        // Combine grids in VBox
+        VBox v1 = new VBox();
+        v1.getChildren().add(grid1);
+        v1.getChildren().add(grid2);
+        v1.setAlignment(Pos.CENTER);
+        // Add Vbox to the main gridpane
+        rootGrid.add(v1, 0, 1);
+    }
 
-		// Set up Grid 1
-		grid1.add(l1, 0, 0);
-		grid1.add(l2, 0, 1);
-		grid1.add(l3, 1, 1);
-		grid1.add(search, 1, 0);
-		GridPane.setHalignment(l1, HPos.RIGHT);
+    /**
+     * Method to draw the friend network of the currently selected person on the
+     * right side of the GUI main screen
+     */
+    void networkBox() {
+        GridPane grid1 = new GridPane();
+        grid1.setPadding(new Insets(10, 10, 10, 10));
+        grid1.setVgap(5);
+        grid1.setHgap(5);
 
-		// Set up Grid 2
-		grid2.add(b1, 0, 2);
-		grid2.add(b2, 0, 3);
-		grid2.add(b3, 0, 4);
-		grid2.add(b4, 0, 5);
-		grid2.setAlignment(Pos.CENTER);
+        // Make these dynamic based on currently selected user and function
+        Label l1 = new Label("Current User: " + currentUser);
+        Label l2 = new Label("Showing: Mutual Friends");
+        //////////////////////////////////////////////////////////////////
 
-		// Combine grids in VBox
-		VBox v1 = new VBox();
-		v1.getChildren().add(grid1);
-		v1.getChildren().add(grid2);
-		v1.setAlignment(Pos.CENTER);
-		// Add Vbox to the main gridpane
-		rootGrid.add(v1, 0, 1);
-	}
+        grid1.add(l1, 0, 0);
+        grid1.add(l2, 0, 1);
 
-	/**
-	 * Method to draw the friend network of the currently selected person on the
-	 * right side of the GUI main screen
-	 */
-	void networkBox() {
-		GridPane grid1 = new GridPane();
-		grid1.setPadding(new Insets(10, 10, 10, 10));
-		grid1.setVgap(5);
-		grid1.setHgap(5);
+        //////////////////////////////////////////////////////////////////
+        // Make displayed data a VBox which is inserted into the Grid
 
-		// Make these dynamic based on currently selected user and function
-		Label l1 = new Label("Current User: " + currentUser);
-		Label l2 = new Label("Showing: Mutual Friends");
-		//////////////////////////////////////////////////////////////////
+        // TODO: determine the functionality to generate a list of friends
+        // and add them to the GUI. Likely will want to use a helper method
+        // which returns a list regardless of if a list or a set is returned,as
+        // the implementation can provide either.
+        // Example functionality shown below
+        // for(Person person : displayList) {
+        //
+        // }
 
-		grid1.add(l1, 0, 0);
-		grid1.add(l2, 0, 1);
+        // Hardcoded display of mutual friends as example
+        Label l3 = new Label("MaryAnn");
+        Label l4 = new Label("JimJohn");
+        Label l5 = new Label("Jimothy");
+        Label l6 = new Label("Janthony");
+        VBox v1 = new VBox();
+        v1.getChildren().addAll(l3, l4, l5, l6);
+        v1.setAlignment(Pos.CENTER);
+        grid1.add(v1, 0, 2);
+        grid1.setAlignment(Pos.CENTER);
 
-		//////////////////////////////////////////////////////////////////
-		// Make displayed data a VBox which is inserted into the Grid
+        rootGrid.add(grid1, 1, 1);
+    }
 
-		// TODO: determine the functionality to generate a list of friends
-		// and add them to the GUI. Likely will want to use a helper method
-		// which returns a list regardless of if a list or a set is returned,as
-		// the implementation can provide either.
-		// Example functionality shown below
-		// for(Person person : displayList) {
-		//
-		// }
+    /**
+     * Draws the status box on the bottom of the main GUI screen
+     */
+    void statusBox() {
 
-		// Hardcoded display of mutual friends as example
-		Label l3 = new Label("MaryAnn");
-		Label l4 = new Label("JimJohn");
-		Label l5 = new Label("Jimothy");
-		Label l6 = new Label("Janthony");
-		VBox v1 = new VBox();
-		v1.getChildren().addAll(l3, l4, l5, l6);
-		v1.setAlignment(Pos.CENTER);
-		grid1.add(v1, 0, 2);
-		grid1.setAlignment(Pos.CENTER);
+    }
 
-		rootGrid.add(grid1, 1, 1);
-	}
+    /**
+     * Class which handles UI interaction with button
+     * 
+     * @author john
+     *
+     */
+    class MyHandler implements EventHandler<ActionEvent> {
+        Button button;
 
-	/**
-	 * Draws the status box on the bottom of the main GUI screen
-	 */
-	void statusBox() {
+        MyHandler(Button button) {
+            this.button = button;
+        }
 
-	}
+        @Override
+        public void handle(ActionEvent e) {
+            if (button.getText().equals("Change to Nature Scene")) {
+                button.setText("Change to headshot");
+            } else
+                button.setText("Change to Nature Scene");
+        }
+    }
 
-	/**
-	 * Class which handles UI interaction with button
-	 * 
-	 * @author john
-	 *
-	 */
-	class MyHandler implements EventHandler<ActionEvent> {
-		Button button;
+    /**
+     * Private class generates an ImageView object from a file
+     * 
+     * @throws FileNotFoundException
+     */
+    private ImageView newImage(String filePath) throws FileNotFoundException {
+        FileInputStream input = new FileInputStream(filePath);
+        Image image = new Image(input);
+        ImageView imageView = new ImageView(image);
+        imageView.setFitWidth(400);
+        imageView.setPreserveRatio(true);
+        return imageView;
 
-		MyHandler(Button button) {
-			this.button = button;
-		}
+    }
 
-		@Override
-		public void handle(ActionEvent e) {
-			if (button.getText().equals("Change to Nature Scene")) {
-				button.setText("Change to headshot");
-			} else
-				button.setText("Change to Nature Scene");
-		}
-	}
+    /**
+     * Displays welcome GUI
+     */
+    private void welcome() {
+        HBox fileNameBox = new HBox();
+        fileNameBox.setSpacing(5);
 
-	/**
-	 * Private class generates an ImageView object from a file
-	 * 
-	 * @throws FileNotFoundException
-	 */
-	private ImageView newImage(String filePath) throws FileNotFoundException {
-		FileInputStream input = new FileInputStream(filePath);
-		Image image = new Image(input);
-		ImageView imageView = new ImageView(image);
-		imageView.setFitWidth(400);
-		imageView.setPreserveRatio(true);
-		return imageView;
+        Button open = new Button("Open");
 
-	}
+        TextField fileName = new TextField();
+        Label label = new Label("Enter file name to read from: ");
 
-	/**
-	 * Displays exit GUI
-	 */
-	private void exiting() {
-		HBox buttons = new HBox();
-		HBox file = new HBox();
+        fileNameBox.getChildren().addAll(label, fileName, open);
 
-		file.setSpacing(5);
+        // confirm -> button.setPadding(new Insets(0, 0, 0, 118));
+        startPane.setPadding(new Insets(10, 10, 10, 10));
 
-		buttons.setPadding(new Insets(5, 5, 5, 5));
-		buttons.setSpacing(5);
+        // startPane.setTop(fileNameBox);
+        startPane.setCenter(fileNameBox);
+    }
 
-		TextField fileName = new TextField("SocialNetworkSave");
-		Label label = new Label("Enter file name to save to: ");
-		Button save = new Button("Save");
-		save.setPadding(new Insets(5, 50, 5, 50));
+    /**
+     * Displays exit GUI
+     */
+    private void exiting() {
+        HBox buttons = new HBox();
+        HBox file = new HBox();
 
-		save.setOnAction(e -> renameConnor(fileName.getText()));
+        file.setSpacing(5);
 
-		Button nSave = new Button("Exit without saving");
-		nSave.setPadding(new Insets(5, 20, 5, 20));
-		nSave.setOnAction(e -> Platform.exit());
+        buttons.setPadding(new Insets(5, 5, 5, 5));
+        buttons.setSpacing(5);
 
-		buttons.getChildren().add(save);
-		buttons.getChildren().add(nSave);
-		file.getChildren().add(label);
-		file.getChildren().add(fileName);
+        TextField fileName = new TextField("SocialNetworkSave");
+        Label label = new Label("Enter file name to save to: ");
+        Button save = new Button("Save");
+        save.setPadding(new Insets(5, 50, 5, 50));
 
-		// Main layout is Border Pane example (top,left,center,right,bottom)
-		exitPane.setPadding(new Insets(10, 10, 10, 10));
-		exitPane.setTop(file);
-		exitPane.setBottom(buttons);
+        save.setOnAction(e -> renameConnor(fileName.getText()));
 
-	}
+        Button nSave = new Button("Exit without saving");
+        nSave.setPadding(new Insets(5, 20, 5, 20));
+        nSave.setOnAction(e -> Platform.exit());
 
-	public void renameConnor(String inputText) {
-		boolean fileSaved = false;
-		fileSaved = socialNetwork.saveLog(inputText);
+        buttons.getChildren().add(save);
+        buttons.getChildren().add(nSave);
+        file.getChildren().add(label);
+        file.getChildren().add(fileName);
 
-		if (!fileSaved) {
-			Alert savedAlert = new Alert(Alert.AlertType.ERROR, "Save unsuccessful");
-			savedAlert.showAndWait();
-		}
+        // Main layout is Border Pane example (top,left,center,right,bottom)
+        exitPane.setPadding(new Insets(10, 10, 10, 10));
+        exitPane.setTop(file);
+        exitPane.setBottom(buttons);
 
-	}
+    }
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		launch(args);
-	}
+    public void renameConnor(String inputText) {
+        boolean fileSaved = false;
+        fileSaved = socialNetwork.saveLog(inputText);
+
+        if (!fileSaved) {
+            Alert savedAlert = new Alert(Alert.AlertType.ERROR, "Save unsuccessful");
+            savedAlert.showAndWait();
+        }
+
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
